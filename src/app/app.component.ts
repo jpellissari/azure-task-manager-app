fb1c140b208345e2e7729b8f49ef56821c9bb62c
João Henrique M Pellissari <pellissari@pop-os.localdomain>
2023-07-11 08:04:10 -0300

    feat: add basic layout to fetch work item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'azure-task-manager-app';
+  workItemNumber: number
+
+  updateWorkItemNumber(event: Event) {
+    this.workItemNumber = parseInt((event.target as HTMLInputElement).value)
+  }
+
+  searchWorkItem() {
+    console.log(this.workItemNumber)
+  }
 }
