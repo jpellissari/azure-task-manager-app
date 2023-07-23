f956969bb27e64f8ff82ff01fceee7bbb4132a57
João Henrique M Pellissari <pellissari@pop-os.localdomain>
2023-07-23 17:53:13 -0300

    feat: add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component'
 import { UsBreakerModule } from './us-breaker/us-breaker.module'
 
+const routes: Routes = [
+  {
+    path: 'us-breaker',
+    loadChildren: () =>
+      import('./us-breaker/us-breaker.module').then((m) => m.UsBreakerModule),
+  },
+  { path: '**', redirectTo: 'us-breaker' },
+]
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UsBreakerModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes), UsBreakerModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/us-breaker/us-breaker.module.ts
+++ src/app/us-breaker/us-breaker.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HttpClientModule } from '@angular/common/http'
+import { RouterModule, Routes } from '@angular/router'
 
 import { TaskCountComponent } from './components/task-count/task-count.component'
 import { WiSearchComponent } from './components/wi-search/wi-search.component'
 import { UsDashboardComponent } from './containers/us-dashboard/us-dashboard.component'
 import { WorkItemService } from './services/work-item.service'
 
+const routes: Routes = [
+  { path: '', component: UsDashboardComponent, pathMatch: 'full' },
+]
+
 @NgModule({
   declarations: [UsDashboardComponent, WiSearchComponent, TaskCountComponent],
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, RouterModule.forChild(routes)],
   providers: [WorkItemService],
 })
 export class UsBreakerModule {}
